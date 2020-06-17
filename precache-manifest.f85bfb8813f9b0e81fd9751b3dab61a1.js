42b53133193c1c41476d58e57aca06545d5e2221
unknown <aneri.patel@theonetehcnologies.co.in>
2020-06-17 11:03:43 +0530

    Updates

---
+++ precache-manifest.f85bfb8813f9b0e81fd9751b3dab61a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879b2e37082b76921093a3649af5d7e8",
+    "url": "/aneriPatel/index.html"
+  },
+  {
+    "revision": "c52b3ab641660983ad95",
+    "url": "/aneriPatel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9e381a92bbda9427447c",
+    "url": "/aneriPatel/static/js/2.e76c46cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aneriPatel/static/js/2.e76c46cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c52b3ab641660983ad95",
+    "url": "/aneriPatel/static/js/main.c27b3ab8.chunk.js"
+  },
+  {
+    "revision": "7e0350bc4b715166022e",
+    "url": "/aneriPatel/static/js/runtime-main.7ec50a3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aneriPatel/static/media/logo.5d5d9eef.svg"
+  }
+]);
